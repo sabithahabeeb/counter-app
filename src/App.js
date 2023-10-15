3848a84cd3eba262bb8cb3c1e38ca00386a72f4c
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-10-15 14:14:54 +0530

    counter-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div >
+     <h1 className='text-center mt-5'>Counter Application</h1>
+     <Counter/>
+    </div>
+  );
+}
+
+export default App;
